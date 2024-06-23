5de1378f97ebbd86b7cf3fcc6a6955c43a95ff4a
sagar reddy <sreddy831509@gmail.com>
2024-06-23 12:54:08 +0000

    incremental

--- models/emp_incremental_model.sql
+++ models/emp_incremental_model.sql
@@ -2,18 +2,15 @@
 {{
     config(
         materialized='incremental',
+        unique_key='id',
         alias='emp_tgt',
-        unique_key='id'
+        on_schema_change='sync_all_columns'
     )
 }}
 
 {% if is_incremental() %}
   -- Incremental logic: only select rows that have been updated or inserted since the last run
-  with new_data as (
-      select * from {{ source('siva', 'EMP_STG') }}
-      where updated_at > (select max(updated_at) from {{ this }})
-  )
-  select * from new_data
+      select * from {{ source('siva', 'EMP_STG') }} where updated_at > (select max(updated_at) from {{ this }})
 {% else %}
   -- Full refresh logic: select all rows from the source table
   select * from {{ source('siva', 'EMP_STG') }}
